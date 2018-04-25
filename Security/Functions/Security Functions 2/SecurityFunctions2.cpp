ae88ec165cfc6fc2923f726040a76441f2ce1c2c
Mustafa Bandukwala <bandukwalamustafa@gmail.com>
2018-04-25 16:17:57 -0700

    Create SecurityFunctions2.cpp

---
+++ Security/Functions/Security Functions 2/SecurityFunctions2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int calculate(int x)
+{
+    return (x*x);
+}
+
+int main() {
+    int x;
+    cin >> x;
+    int result = calculate(x);
+    cout << result << endl;
+    return 0;
+}
